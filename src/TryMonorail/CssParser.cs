7a8b307314a70e90f5480e5f0a3643fc59c471d7
Phil Scott <phil.scott@gmail.com>
2022-11-08 10:29:46 -0500

    removing rx.net

---
+++ src/TryMonorail/CssParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TryMonorail;
+
+internal static partial class CssParser
+{
+    [GeneratedRegex(@"class\s*=\s*[\'\""](?<value>[^<]*?)[\'\""]", RegexOptions.CultureInvariant,
+        matchTimeoutMilliseconds: 1000)]
+    private static partial Regex FindCssClassRegex();
+
+    [GeneratedRegex("<.*class\\s*=\\s*['|\"]([^\"]*)?$", RegexOptions.CultureInvariant,
+        matchTimeoutMilliseconds: 1000)]
+    private static partial Regex IsInCssClassReg();
+
+    public static bool IsInCssClass(string html)
+    {
+        return IsInCssClassReg().IsMatch(html);
+    }
+
+    public static List<string> GetCssClasses(string html)
+    {
+        var matches = FindCssClassRegex().Matches(html);
+        var results = new List<string>(matches.Count);
+        for (var i = 0; i < matches.Count; i++)
+        {
+            results.Add(matches[i].Groups["value"].Captures[0].Value);
+        }
+
+        return results;
+    }
+
+}
